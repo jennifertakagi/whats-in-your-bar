2d1921e2a69d9f4fdd9a8b4f9480c2b3cf1b79f5
jennifertakagi <jennitakagi@gmail.com>
2020-09-25 21:53:57 -0300

    feat: add drink model to DB

---
+++ api/models/DrinkModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const drinkSchema = mongoose.Schema({
+  category:  {
+    type: String,
+    require: true
+  },
+  drink_type: {
+    type: String,
+    require: true
+  },
+  glass_type: {
+    type: String,
+    require: true
+  },
+  image: {
+    type: String,
+    require: true
+  },
+  ingredients: {
+    type: Array,
+    require: true
+  },
+  instructions: {
+    type: String,
+    require: true
+  },
+  measurements_ingredients:  {
+    type: Array,
+    require: true
+  },
+  name: {
+    type: String,
+    require: true
+  }
+});
+
+const drinkModel = mongoose.model('drinks', drinkSchema);
+
+module.exports = drinkModel;
