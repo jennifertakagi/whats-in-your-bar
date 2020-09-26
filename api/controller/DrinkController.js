04ebe4174ffda991c5c8a9231dab0bc618e2fb36
jennifertakagi <jennitakagi@gmail.com>
2020-09-25 21:53:33 -0300

    feat: get all ingredients from DB and filter the drinks by these

--- api/controller/DrinkController.js
+++ api/controller/DrinkController.js
@@ -1,15 +1,67 @@
-//const servicesDB = require('../services/transactionService');
+const servicesDB = require('../services/drinkService');
 
 class DrinkController {
-  async getDrink(request, response, next) {
+  /**
+   * Gets all drinks' ingredients from database
+   * @param {Object} request request object
+   * @param {Object} response response object
+   * @param {Function} next next function
+   */
+  async getAllIngredients(request, response, next) {
     try {
+      const ingredientsDB = await servicesDB.getIngredientsFromDB();
+      
+      response.send({
+        status: 'ok',
+        size: ingredientsDB.length,
+        ingredients: ingredientsDB
+      });
 
-      logger.info(`GET /drink - ${JSON.stringify({})}`);
+      logger.info(`GET /drinks/allIngredients - ${JSON.stringify(ingredientsDB)}`);
     } catch (error) {
         next(error);
     }
   }
 
+  /**
+   * Gets drinks from database that has all the ingredients that user has
+   * @param {Object} request request object
+   * @param {Object} response response object
+   * @param {Function} next next function
+   */
+  async getDrinksByIngredients(request, response, next) {
+    try {
+      const { ingredients } = request.query;
+      const ownIngredients = ingredients.split(',');
+      const drinksDB = await servicesDB.getDrinksByIngredientsFromDB(ownIngredients);
+      const possibleDrinks = await filterDrinks(ownIngredients, drinksDB);
+
+      response.send({
+        status: 'ok',
+        size: possibleDrinks.length,
+        drinks: possibleDrinks
+      });
+
+      logger.info(`GET /drinks/ - ${JSON.stringify(possibleDrinks)}`);
+    } catch (error) {
+        next(error);
+    }
+  }
+}
+
+/**
+ * Filter drinks by ingredients that user has,
+ * returning only the drinks that the use can make (that has all user's ingredients)
+ * @param {Array} ownIngredients the ingredients that user has
+ * @param {Object[]} drinksDB all the drinks that includes at least one user's ingredient
+ * @returns {Object[]} drinks that user can make 
+ */
+function filterDrinks(ownIngredients, drinksDB) {
+  return drinksDB.filter(ingredientsByDrink => {
+    return ingredientsByDrink.ingredients
+      .map(item => ownIngredients.includes(item))
+      .every(ingredient => ingredient)
+})
 }
 
 module.exports = new DrinkController;
