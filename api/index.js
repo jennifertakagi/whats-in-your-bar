15298821b71e590053d3fd96d76be721166c5637
jennifertakagi <jennitakagi@gmail.com>
2020-09-27 16:44:25 -0300

    fix: static to express

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ global.logger = winston.createLogger({
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api/', (_, response) => {
   response.send({
