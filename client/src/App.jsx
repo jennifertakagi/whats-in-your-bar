cdacd1401ce74fa84723c1b9aafface92c6fd732
jennifertakagi <jennitakagi@gmail.com>
2020-09-28 21:22:10 -0300

    feat: create App component and set global styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import MainPage from './pages/MainPage';
 
 import api from './services/api';
+import { mountIngredientsQuery, mountSelectObject } from './helpers/utils';
+import {defaultSelectedValues} from './helpers/defaultSelectedValues';
 
 import './assets/styles/global.css';
-import './styles.css';
 
 export default function App() {
+  const [defaultIngredients, setDefaultIngredients] = useState(defaultSelectedValues);
+  const [drinks, setDrinks] = useState([]);
   const [ingredientsDB, setIngredientsDB] = useState([]);
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
+  const [showLoading, setShowLoading] = useState(false);
+  const [notFoundDrinks, setNotFoundDrinks] = useState(false);
 
   /**
    * Gets all drinks' ingredients from database and
@@ -16,7 +23,9 @@ export default function App() {
     api.get('/drinks/allIngredients')
       .then((response) => {
         const { data = {} } = response;
-        setIngredientsDB(data.ingredients);
+        const ingredientsOptions = mountSelectObject(data.ingredients);
+
+        setIngredientsDB(ingredientsOptions);
       });
   }
 
@@ -24,9 +33,50 @@ export default function App() {
     getIngredientsFromDB();
   }, []);
 
+  /**
+   * Gets the matched drinks from database, and sets them on state
+   */
+  function getDrinksByIngredientsFromDB(buttonClicked) {
+    const queryIngredients = mountIngredientsQuery(selectedIngredients);
+    
+    setShowLoading(buttonClicked);
+    
+    api.get(`/drinks/?ingredients=${queryIngredients}`)
+      .then((response) => {
+        const { data = {} } = response;
+        const drinksDB = data.drinks || [];
+
+        setTimeout(() => {
+          setShowLoading(false);
+          setDrinks(drinksDB);
+
+          if (!drinksDB.length) {
+            setNotFoundDrinks(true);
+          }
+        }, 1000);
+      });
+  }
+
+  /**
+   * Handles with selected ingredients, setting these on state
+   * @param {Array} selectedIngredients list of selected ingredients
+   */
+  function handleSelectIngredients(selectedIngredients) {
+    console.log(selectedIngredients)
+    setSelectedIngredients(selectedIngredients);
+  }
+
   return (
     <div className="container" id="main-page">
-      <h1>What's in your bar?</h1>
+      <MainPage
+        defaultIngredients={defaultIngredients}
+        getDrinksByIngredientsFromDB={getDrinksByIngredientsFromDB}
+        handleSelectIngredients={handleSelectIngredients}
+        ingredients={ingredientsDB}
+        matchedDrinks={drinks}
+        showLoading={showLoading}
+        notFoundDrinks={notFoundDrinks}
+      />
     </div>
   );
 }
